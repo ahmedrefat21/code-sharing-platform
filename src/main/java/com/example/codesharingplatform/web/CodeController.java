d5b9c4dc2ec5a84a1a060b0e620555365f81ffe7
Ahmed Refat <ahmedref21@gmail.com>
2022-05-22 00:56:05 +0200

    add two endpoint to allow user to retrieve code snippet by id or to retrieve the newest 10 snippets that have no restrictions

--- src/main/java/com/example/codesharingplatform/web/CodeController.java
+++ src/main/java/com/example/codesharingplatform/web/CodeController.java
@@ -1,13 +1,38 @@
 package com.example.codesharingplatform.web;
 
+import com.example.codesharingplatform.code.CodeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/code")
 public class CodeController {
+
+    private final CodeService codeService;
+
+    public CodeController(CodeService codeService) {
+        this.codeService = codeService;
+    }
+
+    @GetMapping("/{id}")
+    public String getCode(@PathVariable Long id, Model model) {
+        model.addAttribute("title", "Code");
+        model.addAttribute("code", List.of(this.codeService.getCodeById(id)));
+        return "code";
+    }
+
+    @GetMapping("latest")
+    public String getLatestCode(Model model) {
+        model.addAttribute("title", "Latest");
+        model.addAttribute("code", this.codeService.getLatestCodeSnippets());
+        return "code";
+    }
 
     @GetMapping("new")
     public String newCodeSnippet() {
