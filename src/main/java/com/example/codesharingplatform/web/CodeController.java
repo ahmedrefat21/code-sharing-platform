6540c0eab9bc53847adfc0ff19390e1a1e580e9a
Ahmed Refat <ahmedref21@gmail.com>
2022-05-22 00:41:44 +0200

    add Controller annotation to CodeController class to make spring boot able to discover it in components scanning

--- src/main/java/com/example/codesharingplatform/web/CodeController.java
+++ src/main/java/com/example/codesharingplatform/web/CodeController.java
@@ -1,9 +1,11 @@
 package com.example.codesharingplatform.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @RequestMapping("/code")
 public class CodeController {
 
